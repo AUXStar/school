ddee1e4f6034bfbb78e92d1a5cbe0dde3a1f5c14
njzy <468835121@qq.com>
2024-09-10 23:37:36 +0800

    U

---
+++ models.py
@@ -0,0 +1,22 @@
+from pony.orm import Database, Required, db_session, sum
+from decimal import Decimal
+from time import time
+
+db = Database()
+
+class TransactionDetail(db.Entity):
+    detail = Required(str,500)
+    money = Required(Decimal,10,2)
+    timestamp = Required(int,default=lambda:int(time()))
+
+    @classmethod
+    @db_session
+    def total(cls):
+        return sum(td.money for td in cls)
+    
+
+        
+
+def connect_db(db:Database,db_connect:dict=None):
+    db.bind(**db_connect)
+    db.generate_mapping(create_tables=True)
---
+++ opt.py
@@ -0,0 +1,8 @@
+from models import TransactionDetail,connect_db
+from pony.orm import db_session,select
+
+connect_db()
+
+with db_session:
+    select((td.detail,td.money) for td in TransactionDetail).show()
+    
